cbb87cdbcd1cfd34ceda777941dbf9ba5303340e
Matthew Perkins <mattperkinsee@gmail.com>
2018-08-30 01:57:17 -0400

    added timer

--- js/app.js
+++ js/app.js
@@ -14,8 +14,7 @@ var stars = 3;
 var scorePanel = document.querySelector('.score-panel');
 var header = document.querySelector('header');
 var checkMark = document.querySelector('.checkmark');
-var seconds =0;
-var minutes= 0;
+var timer = new Timer();
 //
 //var cardList = $('.deck > .card > i');
 
@@ -30,11 +29,10 @@ var minutes= 0;
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function timer() {
-    t = setTimeout(add, 1000);
-}
-timer();
-
+timer.start();
+timer.addEventListener('secondsUpdated', function (e) {
+    $('#basicUsage').html(timer.getTimeValues().toString());
+});
 shuffle(cards);
 buildCardsHTML();
 
@@ -59,20 +57,16 @@ function shuffle(array) {
 var resetButton = document.getElementsByClassName('restart');
 resetButton[0].addEventListener('click', reset);
 
-
 function buildCardsHTML() {
+    
     for (let i = 0; i < cards.length; i++) {
         let deck = document.querySelector(".deck");
-        var liElement = document.createElement('li');
+        let liElement = document.createElement('li');
         deck.appendChild(liElement);
         let cardName = cards[i];
         liElement.className = 'card fa fa-' + cardName + ' hide';
         liElement.addEventListener('click', displayCard);
-
-
     }
-   
-
 }
 
 
@@ -91,7 +85,7 @@ function displayCard(e) {
 
 
 function matchCheck(e) {
-    //    e.target.classList.add("show");
+//    e.target.classList.add("show");
     targetsList.push(e.target);
     if (openCardsList.length < 2) {
         openCardsList.push(e.target.className);
@@ -197,7 +191,6 @@ function determineStars() {
 
     }
 }
-
 function displayWin(movement, stars) {
     /*remove cards and hide header*/
     scorePanel.classList.add('hide');
@@ -228,6 +221,11 @@ function displayWin(movement, stars) {
     subtitle.innerHTML = "With " + movement + " moves and " + stars + " stars! Wooo!";
     button.innerHTML = "Play again!";
     button.addEventListener('click', removeWinScreen);
+    
+    
+   
+   timer.stop();
+
 
 }
 
@@ -248,13 +246,14 @@ function reset() {
     movement = 0;
     stars = 3;
     shuffle(cards);
-    clearArrays();
+    targetsList = [];
+    openCardsList = [];
     destroyCards();
     lockedCards = [];
 
     var $resetStars = $('.stars > li > i');
     $resetStars.removeClass('fa fa-star-o').addClass('fa fa-star');
-
+    timer.reset();
     buildCardsHTML();
     console.log('reset');
 
@@ -268,6 +267,9 @@ function destroyCards() {
 
 
 }
+
+
+
 
 
 
